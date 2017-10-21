b5d643d20877cabb8aebbf09a510b6d1340c5ef3
aaron <edougher@students.mchenry.edu>
2017-10-20 20:09:13 -0500

    Work in progress.

--- src/com/company/EmployeeClass.java
+++ src/com/company/EmployeeClass.java
@@ -2,31 +2,31 @@ package com.company;
 
 
 public class EmployeeClass {
-    private double hoursIn;
+
     private String name;
-    private String position;
+    private PositionTitle position;
     private boolean salary = true;
     private double payRate;
     private int shift;
     private String startDate;
-    private double weeklyTotal = payRate*hoursIn;
+    private double weeklyTotal;
 
 
-    public EmployeeClass(String name, String position, boolean salary, double payRate, int shift, String startDate, double weeklyTotal) {
+    public EmployeeClass(String name, PositionTitle position, boolean salary, double payRate, int shift, String startDate) {
         this.name = name;
         this.position = position;
         this.salary = salary;
         this.payRate = payRate;
         this.shift = shift;
         this.startDate = startDate;
-        this.weeklyTotal = weeklyTotal;
+
     }
 
-    public String getPosition() {
+    public PositionTitle getPosition() {
         return position;
     }
 
-    public void setPosition(String position) {
+    public void setPosition(PositionTitle position) {
         this.position = position;
     }
 
@@ -50,10 +50,7 @@ public class EmployeeClass {
         return shift;
     }
 
-    public void setShift(int shift) {
-        this.shift = shift;
-        
-    }
+    public void setShift(int shift) { this.shift = shift; }
 
     public String getStartDate() {
         return startDate;
@@ -62,31 +59,41 @@ public class EmployeeClass {
     public void setStartDate(String startDate) {
         this.startDate = startDate;
     }
-    public double getWeeklyTotal(){
+
+
+    public double calculate(double hoursIn){
+        double overTimePay = 0;
+        double overTimeHrs = hoursIn - 40;
+        double overTimeTotal = overTimeHrs*overTimePay;
+        if (hoursIn > 40 && salary == false) {
+            overTimePay = payRate*1.5;
+        }
+
+
+
+        switch (getShift())
+        {
+            case 1:
+                return weeklyTotal = payRate * (hoursIn + overTimeHrs);
+            case 2:
+                return weeklyTotal = payRate * hoursIn + .05;
+            case 3:
+                return weeklyTotal = payRate * hoursIn + .10;
+        }
+
+
+
         return weeklyTotal;
     }
-    public void setWeeklyTotal(double weeklyTotal) {
-        this.weeklyTotal = weeklyTotal;
+
+
+
+    public void display() {
+
+        System.out.println(weeklyTotal);
     }
 
-   public double calculate (double hoursIn) {
-        
 
-        if (salary == false && hoursIn > 40) payRate = payRate * 1.5;
-
-        if (shift == 2) {
-            weeklyTotal = weeklyTotal + .05;
-        } if (shift == 3) {
-            weeklyTotal = weeklyTotal + .10;
-        }
-       return weeklyTotal;
-    }
-    public void display(double hoursIn) {
-       double pay = payRate*hoursIn;
-        System.out.println( name + "\n" + position + salary + payRate + shift + startDate + weeklyTotal);
-
-    }
-    
 }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        EmployeeClass emp1 = new EmployeeClass("aaron", "who knows", false, 27.00, 2, "12/12/12", 0);
+        EmployeeClass emp1 = new EmployeeClass("aaron", PositionTitle.Administration, false, 27.00, 3, "12/12/12");
 
 
-        emp1.display(45);
-        emp1.calculate(45);
+        emp1.display();
+        emp1.calculate( 40);
 
     }
 }
